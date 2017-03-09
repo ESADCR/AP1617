041de733d05dcaea794af845bc5ed9b270b642cd
RodrigoAntunesSantos <rodrigoantunessantos07@hotmail.com>
2017-03-09 23:30:04 +0000

    segundo exercicio

---
+++ rodrigo/Exercicio2/sketch.js
@@ -0,0 +1,17 @@
+var totalPts = 300;
+var steps = totalPts + 1;
+  
+function setup() {
+  createCanvas(710, 400);
+  stroke(255);
+  frameRate(1);
+} 
+
+function draw() {
+  background(0);
+  var rand = 0;
+  for  (var i = 1; i < steps; i++) {
+    point( (width/steps) * i, (height/2) + random(-rand, rand) );
+    rand += random(-5, 5);
+  }
+}
