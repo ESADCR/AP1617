75a812a67c404b6f416998a76deaa24b38dfbfce
Inês Lopes <Ins Lopes>
2017-03-10 15:37:22 +0000

    aula3

--- ines/Exercicio 2/sketch.js
+++ ines/Exercicio 2/sketch.js
@@ -1,11 +1,19 @@
 
-
+var barWidth = 20;
+var lastBar = -1;
 
 function setup() {
-
- 
+  createCanvas(720, 400);
+  colorMode(HSB, width, height, 100); 
+  noStroke();
 }
 
 function draw() {
- 
+  var whichBar = mouseX / barWidth;
+  if (whichBar != lastBar) {
+    var barX = whichBar * barWidth;
+    fill(barX, mouseY, 66);
+    rect(barX, 0, barWidth, height);
+    lastBar = whichBar;
+  }
 }
