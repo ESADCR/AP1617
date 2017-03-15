8ecad563d854174d290a972332f4533115c6b9c8
JoanaLopes98 <joanamlopes98@gmail.com>
2017-03-15 22:55:57 +0000

    alteraçao8

--- joana/aula 3/sketch.js
+++ joana/aula 3/sketch.js
@@ -9,5 +9,12 @@ function setup() {
 }
 
 function draw() {
- 
+ 	line(30, 20, 85, 75);
+ 	ellipse(56, 46, 55, 55);
+ 	rect(30, 20, 55, 55, 20);
+ 	triangle(30, 75, 58, 20, 86, 75);
+}
+
+function mouseClicked(){
+	background(255);	
 }
