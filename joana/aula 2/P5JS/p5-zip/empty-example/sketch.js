f55f259a906d94850c92e6a40da7c607a206089f
JoanaLopes98 <joanamlopes98@gmail.com>
2017-03-16 11:36:36 +0000

    alteraçao9

--- joana/aula 2/P5JS/p5-zip/empty-example/sketch.js
+++ joana/aula 2/P5JS/p5-zip/empty-example/sketch.js
@@ -1,5 +1,5 @@
 
-
+/*
 var img;
 var smallPoint, largePoint;
 
@@ -25,4 +25,39 @@ function draw() {
   var pix = img.get(x, y);
   fill(pix, 128);
   ellipse(x, y, pointillize, pointillize);
+}265 margin left
+*/
+
+
+var x;
+var y;
+var outsideRadius = 200;
+var insideRadius = 100;
+
+function setup() {
+  createCanvas(720, 400);
+  background(204);
+  x = width/2;
+  y = height/2;
+}
+
+function draw() {
+  background(204);
+  
+  var numPoints = int(map(mouseX, 0, width, 6, 60));
+  var angle = 0;
+  var angleStep = 360.0/numPoints;
+    
+  beginShape(TRIANGLE_STRIP); 
+  for (var i = 0; i <= numPoints; i++) {
+    var px = x + cos(radians(angle)) * outsideRadius;
+    var py = y + sin(radians(angle)) * outsideRadius;
+    angle += angleStep;
+    vertex(px, py);
+    px = x + cos(radians(angle)) * insideRadius;
+    py = y + sin(radians(angle)) * insideRadius;
+    vertex(px, py); 
+    angle += angleStep;
+  }
+  endShape();
 }
