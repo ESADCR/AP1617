9c162fd9795c3f2b271dbb5b061c8dc2e48ead7b
helenaisabel <3160077@my.ipleiria.pt>
2017-03-24 15:15:42 +0000

    atualizar

---
+++ helena/aula04/ex04/sketch.js
@@ -0,0 +1,22 @@
+
+var passo = 0;
+
+function setup() {
+
+createCanvas(800, 600);
+background(255);
+
+Fill(0)
+
+
+
+}
+
+function draw() {
+background(250);
+
+ellipse(width/2,height/2,20,20);
+passo = passo+1;
+
+ 
+}
