a20fd8d57d1f755748588c06709b7b178c118279
daniel <limadaniel121998@gmail.com>
2017-04-06 14:17:03 +0100

    modificacoes

--- daniel/exercicio6/sketch.js
+++ daniel/exercicio6/sketch.js
@@ -1,6 +1,9 @@
 var ballX = 300;
 var ballY = 300;
 var ballSPD = 2;
+var ballX1 = 20;
+var ballY1 = 225;
+var ballSPD1 = 5;
 
 function setup() {
 	createCanvas(600, 600);
@@ -10,17 +13,26 @@ function draw() {
 	background(51);
 	fill(255);
 	ellipse(ballX, ballY, 30, 30);
-	/* ellipse(ballX1, ballY1, 30, 30); */
+	ellipse(ballX1, ballY1, 30, 30);
 
 	ballX += ballSPD;
 
-	text("EXERCICIO PONG", 230, 230);
+	text("EXERCICIO PONG", 225, 230);
 	if (ballX > 600 - 30 / 2) {
 		ballSPD = -ballSPD;
 	}
 	text("Se tu o desejas, podes voar, só tens de confiar muito em ti.", 130, 305);
 	if (ballX < 0 + 30 / 2) {
 		ballSPD = -ballSPD;
+	}
+
+	ballX1 += ballSPD1;
+
+	if (ballX1 > 600 - 30 / 2) {
+		ballSPD1 = -ballSPD1;
+	}
+	if (ballX1 < 0 + 30 / 2) {
+		ballSPD1 = -ballSPD1;
 	}
 
 }
