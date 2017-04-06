76c46fd6d556033895df57a3d1d533f78ee4cebe
Inês Lopes <Ins Lopes>
2017-04-07 00:05:34 +0100

    ex6

--- ines/Exercicio 5/sketch.js
+++ ines/Exercicio 5/sketch.js
@@ -1,8 +1,56 @@
-
+var batatas;
+var raio;
+var valor;
+var decisao;
 
 function setup() {
-	createCanvas(800, 600);
+
+  createCanvas(950, 700);
+    raio = 0.00;
+    decisao = true;
+      background(200);
+      stroke(30, 72, 72);
+      strokeWeight(2);
+
+
 }
 
-
 function draw() {
+
+  background (200);   
+
+    if (decisao == false){  
+      stroke(30, 72, 72);
+      fill(0);
+    if (mouseX <= height/2) {
+      raio = map(mouseY,70,width,0,30);}
+    else {
+      raio = map(mouseY,70,width,0,70);
+    }}
+
+  else{
+
+    fill(30,72,72);
+      if (mouseY >= height/2) {
+        raio = map(mouseX,50,width,20,200);}
+
+    else {
+    raio = map(mouseX,50,width,80,200);
+    }
+  }
+
+  strokeWeight(0);
+  ellipse (width/4,height/4,raio,raio);
+    batatas = int(raio);
+    valor =  str(raio);
+      for (var i = 1 ; i <= 1 ; i++) {
+      for (var j = 1 ; j < 4 ; j++) {
+  ellipse(i*width/4,j*height/4,raio*(j+i),raio*(j+i));
+
+  }}
+}
+
+function keyTyped(){
+  if (key == 'a'){
+    decisao = !decisao;}
+}
---
+++ ines/Exercicio 6/sketch.js
@@ -0,0 +1,37 @@
+var[] mensagens = {"batatas", "feijao", "maca", "acelga", "rucola"};
+var mensagemBon = "comida";
+var randomizeX;
+var randomizeY;
+var counter = 0;
+var limite = 10;
+
+
+function setup(){
+
+  createCanvas(800,600);
+  frameRate();
+  colorMode(RGB);
+  background(0);
+  fill(250,10,40);
+  textSize(40);
+  textAlign(CENTER);
+}
+
+
+function draw(){
+
+  background(0);
+  counter = 0;
+  
+  while(counter <= limite) {
+    
+    for (var i = 0; i< mensagens.length; i++) {
+      randomizeX = random(2*width/4);
+      randomizeY = random(2*height/4);
+      text(mensagens[i],width/4+randomizeX,height/4+randomizeY);
+    }
+    
+    counter++;
+    fill(random(200),10,random(200));
+  }
+}
