5f1a7e1c5969e570bb23be73d1c37c93548fbdc4
lopestiago95 <tiagofilipepl@hotmail.com>
2017-04-06 17:41:29 +0100

    3 em 3...

---
+++ tiago/ex4/sketch.js
@@ -0,0 +1,33 @@
+var movimentox = 50;
+var movimentoy = 50;
+var pi = 3.14;
+
+
+function setup() {
+  createCanvas(600,400);
+  background(0,0,0);
+}
+
+function draw() {
+  background(50,20,50);
+  fill(20,26,200);
+  arc(movimentox, movimentoy, 80, 80, 0, pi+QUARTER_PI, PIE);
+
+
+
+  movimentox = movimentox + 1;
+  pi = pi + 0.1;
+
+  {
+    if (movimentox >= 200){
+    (movimentoy = movimentoy + 0.5)
+    }
+  }
+
+  {
+    if (movimentoy >= 200){
+      (movimentox = 550)
+    }
+  }
+}
+
