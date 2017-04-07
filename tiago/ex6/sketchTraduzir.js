d7a17f7872d8547ae23b8e938020f1a21a6ab22c
lopestiago95 <3160068@my.ipleiria.pt>
2017-04-07 11:02:36 +0100

    melhor agora

--- tiago/ex6/sketchTraduzir.js
+++ tiago/ex6/sketchTraduzir.js
@@ -1,5 +1,5 @@
-var[] mensagens = {"batatas", "feijao", "maca", "acelga", "rucola"};
-var mensagemBon = " comida";
+var mensagens = ["azul", "verde", "vermelho", "amarelo", "laranja"];
+var mensagemBon = " cores";
 var randomizeX;
 var randomizeY;
 var counter = 0;
@@ -9,9 +9,9 @@ var limite = 10;
 function setup(){
 
   createCanvas(800,600);
-  frameRate();
+  frameRate(15);
   colorMode(RGB);
-  background(0);
+  background(51);
   fill(250,10,40);
   textSize(40);
   textAlign(CENTER);
@@ -20,17 +20,19 @@ function setup(){
 
 function draw(){
 
-  background(0);
+  background(51);
   counter = 0;
-  
+
   while(counter <= limite) {
-    
+
     for (var i = 0; i< mensagens.length; i++) {
       randomizeX = random(2*width/4);
       randomizeY = random(2*height/4);
       text(mensagens[i],width/4+randomizeX,height/4+randomizeY);
+
     }
     counter++;
     fill(random(200),10,random(200));
+
   }
 }
